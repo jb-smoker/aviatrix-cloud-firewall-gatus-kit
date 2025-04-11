385ea8c8ba0e0fe8d4bc0640e1d834e5975699a3
John Smoker <jsmoker@aviatrix.com>
2025-04-11 12:39:55 -0700

    fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "aws_dashboard_public_ip" {
-  value = contains(var.clouds, "aws") ? module.aws["aws"].dashboard_public_ip : null
+  value = contains([for s in var.clouds : lower(s)], "aws") ? module.aws["aws"].dashboard_public_ip : null
 }
 
 output "azure_dashboard_public_ip" {
-  value = contains(var.clouds, "azure") ? module.azure["azure"].dashboard_public_ip : null
+  value = contains([for s in var.clouds : lower(s)], "azure") ? module.azure["azure"].dashboard_public_ip : null
 }
 
 output "local_user_password" {
-  value       = var.local_user_password != null ? null : random_password.password[0].result
+  value       = var.local_user_password != null ? var.local_user_password : random_password.password[0].result
   sensitive   = true
   description = "The generated random local_user_password"
 }
